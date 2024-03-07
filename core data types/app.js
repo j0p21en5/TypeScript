15fd6c85d35bfa3dad734fe3f2ce2f64720540b1
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-03-07 23:35:53 +0530

    update

---
+++ core data types/app.js
@@ -0,0 +1,10 @@
+function dummy(a) {
+    return a;
+}
+var result = dummy('hello');
+console.log(result);
+function add(c, d) {
+    return c + d;
+}
+var sum = add(2, 3);
+console.log(sum);
---
+++ core data types/app.ts
@@ -0,0 +1,15 @@
+function dummy(a:string){
+    return a;
+} 
+
+let result = dummy('hello')
+
+console.log(result);
+function add(c,d){
+    return c+d;
+
+}
+
+let sum = add(2,3);
+
+console.log(sum);
