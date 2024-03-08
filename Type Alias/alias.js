bc4750c7f1d59c3c9684732311afd515fd3c7022
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-03-09 00:32:05 +0530

    update

---
+++ Type Alias/alias.js
@@ -0,0 +1,4 @@
+var a = 1;
+var b = "chukku";
+console.log(a);
+console.log(b);
---
+++ Type Alias/alias.ts
@@ -0,0 +1,10 @@
+type varType = string | number | undefined ;
+
+let a : varType = 1 ;
+
+let b : varType = "chukku";
+
+
+console.log(a);
+
+console.log(b);
