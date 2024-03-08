eecb1dca99499f694ccea44ec2567c8d07dc5b83
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-03-08 16:23:14 +0530

    add code
    type inference in typescript

---
+++ Type Inference/aap.js
@@ -0,0 +1,10 @@
+var data = 5;
+data = 6;
+data = 10;
+// data = 'hi';
+console.log(data);
+var fix = 5;
+// fix=20;
+console.log(fix);
+var a = 5;
+// a=6;
---
+++ Type Inference/aap.ts
@@ -0,0 +1,18 @@
+let data=5;
+data = 6;
+data = 10;
+// data = 'hi';
+
+console.log(data);
+
+
+let fix:5=5;
+// fix=20;
+
+console.log(fix);
+
+
+const a= 5;
+// a=6;
+
+
