8ef48864c3a1fc17ce440295b0e5eb679e571e71
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-03-08 17:56:43 +0530

    update

---
+++ Array type/array.js
@@ -0,0 +1,8 @@
+var arr = ['php', 'java', 'node', 'js'];
+var arr2 = [1, 5, 4, 50];
+var random = [123, 'name', true];
+var random2 = [true, 234, 'jay'];
+console.log(arr);
+console.log(arr2);
+console.log(random);
+console.log(random2);
---
+++ Array type/array.ts
@@ -0,0 +1,16 @@
+let arr : string[]=['php','java','node','js'];
+
+let arr2:number[]=[1,5,4,50];
+
+let random=[123,'name',true];
+
+let random2 : any[] = [true,234,'jay'];
+
+console.log(arr);
+console.log(arr2);
+console.log(random);
+console.log(random2);
+
+
+
+
