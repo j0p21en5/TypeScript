472ee3dda312b6ab0ccca21e50501f5bd4a74913
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-03-09 19:00:47 +0530

    update

---
+++ Unknown Type/unknown.ts
@@ -0,0 +1,16 @@
+let data:unknown;
+
+data = 30;
+data = "hello";
+// data = true;
+
+let item :string;
+
+// item = data;   it is not possible 
+
+if (typeof data === 'string')
+
+{
+    item = data;
+
+}
