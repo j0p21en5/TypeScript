472a922c0f7161619d4c7f6918bc44fdb7a22ec4
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-03-10 18:09:53 +0530

    update
    add class in typescript

--- Class In TypeScript/class.js
+++ Class In TypeScript/class.js
@@ -1,6 +1,13 @@
 "use strict";
 class Users {
+    constructor() {
+        this.name = '';
+        this.email = '';
+    }
     addUser(user) {
         return `${user} is added`;
     }
 }
+let user1 = new Users;
+let result = user1.addUser("Jay");
+console.log(result);
--- Class In TypeScript/class.ts
+++ Class In TypeScript/class.ts
@@ -1,8 +1,12 @@
 class Users{
-    name:'';
-    email:'';
+    name='';
+    email ='';
 
     addUser(user){
         return `${user} is added`
     }
 }
+
+let user1 = new Users;
+let result = user1.addUser("Jay");
+console.log(result);
